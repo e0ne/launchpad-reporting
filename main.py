f1789baee31ab2aa23ee4d44522be19e2f76ff65
Ivan Kolodyazhny <e0ne@e0ne.info>
2014-07-06 15:46:47 +0300

    Multiproject support fix for all pages

--- main.py
+++ main.py
@@ -13,16 +13,31 @@ lpdata = LaunchpadData()
 @app.route('/project/<project_name>/bug_table_for_status/<bug_type>/<milestone_name>')
 def bug_table_for_status(project_name, bug_type, milestone_name):
     project = lpdata.get_project(project_name)
-    return flask.render_template("bug_table.html", project=project)
+    available_projects = settings.AVAILABLE_PROJECTS
+    return flask.render_template("bug_table.html",
+                                 project=project,
+                                 available_projects=available_projects)
+
 
 @app.route('/project/<project_name>/bug_table_for_status/<bug_type>/<milestone_name>/bug_list')
 def bug_list(project_name, bug_type, milestone_name):
     project = lpdata.get_project(project_name)
+    available_projects = settings.AVAILABLE_PROJECTS
     tags = None
     if 'tags' in flask.request.args:
         tags = flask.request.args['tags'].split(',')
-    bugs = lpdata.get_bugs(project_name, LaunchpadData.BUG_STATUSES[bug_type], milestone_name, tags)
-    return flask.render_template("bug_list.html", project=project, bugs=bugs, bug_type=bug_type, milestone_name=milestone_name, selected_bug_table=True)
+    bugs = lpdata.get_bugs(project_name,
+                           LaunchpadData.BUG_STATUSES[bug_type],
+                           milestone_name,
+                           tags)
+    return flask.render_template("bug_list.html",
+                                 project=project,
+                                 available_projects=available_projects,
+                                 bugs=bugs,
+                                 bug_type=bug_type,
+                                 milestone_name=milestone_name,
+                                 selected_bug_table=True)
+
 
 @app.route('/project/<project_name>')
 def project_overview(project_name):
@@ -30,27 +45,39 @@ def project_overview(project_name):
     available_projects = settings.AVAILABLE_PROJECTS
     return flask.render_template("project.html",
                                  project=project,
-                                 available_projects = available_projects,
+                                 available_projects=available_projects,
                                  selected_overview=True)
+
 
 @app.route('/project/<project_name>/bug_trends/<milestone_name>')
 def bug_trends(project_name, milestone_name):
     project = lpdata.get_project(project_name)
-    return flask.render_template("bug_trends.html", project=project, milestone_name=milestone_name, selected_bug_trends=True)
+    available_projects = settings.AVAILABLE_PROJECTS
+    return flask.render_template("bug_trends.html",
+                                 project=project,
+                                 available_projects=available_projects,
+                                 milestone_name=milestone_name,
+                                 selected_bug_trends=True)
+
 
 @app.route('/project/<project_name>/api/release_chart_trends/<milestone_name>/get_data')
 def bug_report_trends_data(project_name, milestone_name):
     data = ReleaseChart(lpdata, project_name, milestone_name).get_trends_data()
     return flask.json.dumps(data)
 
+
 @app.route('/project/<project_name>/api/release_chart_incoming_outgoing/<milestone_name>/get_data')
 def bug_report_get_incoming_outgoing_data(project_name, milestone_name):
-    data = ReleaseChart(lpdata, project_name, milestone_name).get_incoming_outgoing_data()
+    data = ReleaseChart(lpdata,
+                        project_name,
+                        milestone_name).get_incoming_outgoing_data()
     return flask.json.dumps(data)
+
 
 @app.route('/')
 def main_page():
-    return flask.redirect(flask.url_for("project_overview", project_name=settings.PROJECT_NAME))
+    return flask.redirect(flask.url_for("project_overview",
+                                        project_name=settings.PROJECT_NAME))
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 4444, threaded = True, debug = True)
+    app.run(host="0.0.0.0", port=4444, threaded=True, debug=True)
